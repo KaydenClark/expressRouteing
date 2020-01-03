d859560ca881c89af4ca3726335bba5ed84cf99b
Kayden Clark <kaydenclark725@gmail.com>
2020-01-03 01:08:15 -0700

    saving working code

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const router = express.Router();
 
 router.use(cors())
+router.use(express.json())
 
 router.use('/lists', require('./lists/listsAPI'))
 router.use('/tasks', require('./tasks/tasksAPI'))
